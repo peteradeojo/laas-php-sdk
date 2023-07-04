e56492f688ecef1d8c239618009b6941bc967216
Peter Ade-Ojo <peteradeojo@outlook.com>
2023-07-04 10:46:19 +0100

    fix: fixed clashing env function

--- src/util.php
+++ src/util.php
@@ -1,5 +1,7 @@
 <?php
 
-function env(string $key, $default = null) {
-  return $_ENV[$key] ?? $default;
+if (!function_exists('env')) {
+  function env(string $key, $default = null) {
+    return $_ENV[$key] ?? $default;
+  }
 }
